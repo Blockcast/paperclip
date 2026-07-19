8dd1a74642d8343bbfbf028781c793c414723e5a
Omar Ramadan <omar@blockcast.net>
2026-07-19 03:12:41 +0000

    test(server): tolerate ARC database startup delays

--- server/src/__tests__/attention-service.test.ts
+++ server/src/__tests__/attention-service.test.ts
@@ -52,7 +52,7 @@ describeEmbeddedPostgres("attention service", () => {
   beforeAll(async () => {
     tempDb = await startEmbeddedPostgresTestDatabase("paperclip-attention-service-");
     db = createDb(tempDb.connectionString);
-  }, 30_000);
+  }, 120_000);
 
   afterEach(async () => {
     await db.delete(inboxDismissals);
